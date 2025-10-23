c2dddf0892b3b8e54c1547ab03ebec6e0152411f
labe2301 <labe2301@student.miun.se>
2025-10-23 13:15:29 +0200

    [refactor] Resolve code style errors to pass CI checks

--- src/test/java/com/thealgorithms/ciphers/PlayfairTest.java
+++ src/test/java/com/thealgorithms/ciphers/PlayfairTest.java
@@ -1,15 +1,22 @@
 package com.thealgorithms.ciphers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class PlayfairTest {
+    private PlayfairCipher playfairCipher;
+    private final String keyword = "KEYWORD";
+
+    @BeforeEach
+    public void setup() {
+        playfairCipher = new PlayfairCipher(keyword);
+    }
 
     @Test
     public void testEncryption() {
-        PlayfairCipher playfairCipher = new PlayfairCipher("KEYWORD");
-
         String plaintext = "HELLO";
         String encryptedText = playfairCipher.encrypt(plaintext);
         assertEquals("GYIZSC", encryptedText);
@@ -17,8 +24,6 @@ public class PlayfairTest {
 
     @Test
     public void testDecryption() {
-        PlayfairCipher playfairCipher = new PlayfairCipher("KEYWORD");
-
         String encryptedText = "UDRIYP";
         String decryptedText = playfairCipher.decrypt(encryptedText);
         assertEquals("NEBFVH", decryptedText);
@@ -26,12 +31,29 @@ public class PlayfairTest {
 
     @Test
     public void testEncryptionAndDecryption() {
-        PlayfairCipher playfairCipher = new PlayfairCipher("KEYWORD");
-
         String plaintext = "PLAYFAIR";
         String encryptedText = playfairCipher.encrypt(plaintext);
         String decryptedText = playfairCipher.decrypt(encryptedText);
 
         assertEquals(plaintext, decryptedText);
+    }
+
+    @Test
+    void shouldEncryptAndDecryptDuringSameRowDigraph() {
+        String plaintext = keyword.substring(0, 2);
+
+        String encrypted = playfairCipher.encrypt(plaintext);
+        String decrypted = playfairCipher.decrypt(encrypted);
+
+        assertEquals(plaintext, decrypted, "Should not decrypt to the same letters");
+    }
+
+    @Test
+    void shouldPadOddLengthplaintext() {
+        String plaintext = "cat";
+
+        String encrypted = playfairCipher.encrypt(plaintext);
+
+        assertTrue(encrypted.length() % 2 == 0, "Should be even length");
     }
 }
